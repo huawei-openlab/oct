1f7776d84afbdc9092a5021419fefbc127a3718b
Tiesheng <chengtiesheng@huawei.com>
2015-07-25 21:05:50 +0800

    Update network-config-pipework.sh

--- Cases/Network/source/network-config-pipework.sh
+++ Cases/Network/source/network-config-pipework.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 # This code should (try to) follow Google's Shell Style Guide
 # (https://google-styleguide.googlecode.com/svn/trunk/shell.xml)
+git clone https://github.com/jpetazzo/pipework
+cd pipework/
+cp pipework /usr/bin
+
+pipework br0 $container 172.28.1.2/24@172.28.1.1
+brctl addif br0 eth0
+ip addr add 172.28.1.1/24 dev br0
