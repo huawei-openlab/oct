58c7f76abf022e9676846f446543dc30fa097abb
Tiesheng <chengtiesheng@huawei.com>
2015-07-25 20:58:48 +0800

    Create iperf_Client.go

---
+++ Cases/Network/source/iperf_Client.go
@@ -0,0 +1,47 @@
+// Copyright 2014 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+       "fmt"
+       "log"
+       "os/exec"
+)
+
+func init() {
+
+}
+
+func iperf_Client() {
+    //exec shell in host machine to get docker container id
+    cmd := exec.Command("/bin/sh", "-c", "docker ps -q")
+    short_id, err := cmd.Output()
+
+    cmd = exec.Command("/bin/sh", "-c", "docker inspect -f '{{.id}}' "+string(short_id))
+    full_id, err := cmd.Output()
+    fmt.Println("The container id of iperf server docker is %v", string(full_id))
+  
+    //Container name : "/docker/container id"
+    containerName := "/docker/" + string(full_id)
+
+    //Enter the container and exec iperf Client command
+    exec.Command("/bin/sh", "-c", "docker attach %v", string(full_id))
+
+    exec.Command("/bin/sh", "-c", "iperf -c %v -u -i 1 -b 200M", IperfServer)
+}
+
+func main() {
+    iperf_Client()	
+}
