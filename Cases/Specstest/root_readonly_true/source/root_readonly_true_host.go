8c7550fa9aa3b81373f73a4468b03bb4d72c7c82
zenlin <linzhinan@huawei.com>
2015-08-04 10:22:52 +0800

    Split the host env setting job to the public pkg Specstest/source/hostsetup/host_setup.go.

--- Cases/Specstest/root_readonly_true/source/root_readonly_true_host.go
+++ Cases/Specstest/root_readonly_true/source/root_readonly_true_host.go
@@ -16,67 +16,21 @@ package main
 
 import (
 	configconvert "./../../source/configconvert"
+	hostsetup "./../../source/hostsetup"
 	"fmt"
 	specs "github.com/opencontainers/specs"
 	"log"
-	"os/exec"
 )
 
 func testRootReadonlyTrue() {
 
-	cmd := exec.Command("/bin/sh", "-c", "go build root_readonly_true_guest.go")
-	_, err := cmd.Output()
+	var guestProgrammeFile string
+	guestProgrammeFile = "root_readonly_true_guest"
+	err := hostsetup.SetupEnv(guestProgrammeFile)
 	if err != nil {
-		log.Fatalf("Specstest root readonly test: build guest programme error, %v", err)
+		log.Fatalf("Specstest root readonly test: hostsetup.SetupEnv error, %v", err)
 	}
-
-	cmd = exec.Command("/bin/sh", "-c", "mkdir -p /tmp/testtool")
-	_, err = cmd.Output()
-	if err != nil {
-		log.Fatalf("Specstest root readonly test: mkdir testtool dir error, %v", err)
-	}
-
-	cmd = exec.Command("/bin/sh", "-c", "mv root_readonly_true_guest /tmp/testtool/")
-	_, err = cmd.Output()
-	if err != nil {
-		log.Fatalf("Specstest root readonly test: mv guest programme error, %v", err)
-	}
-
-	cmd = exec.Command("/bin/sh", "-c", "touch  /tmp/testtool/readonly_true_out.txt")
-	_, err = cmd.Output()
-	if err != nil {
-		log.Fatalf("Specstest root readonly test: touch readonly_true_out.txt err, %v", err)
-	}
-
-	cmd = exec.Command("/bin/sh", "-c", "chown root:root  /tmp/testtool/readonly_true_out.txt")
-	_, err = cmd.Output()
-	if err != nil {
-		log.Fatalf("Specstest root readonly test: change to root power err, %v", err)
-	}
-
-	cmd = exec.Command("/bin/sh", "-c", "docker pull ubuntu:14.04")
-	_, err = cmd.Output()
-	if err != nil {
-		log.Fatalf("Specstest root readonly test: pull image error, %v", err)
-	}
-
-	cmd = exec.Command("/bin/sh", "-c", "docker export $(docker create ubuntu) > ubuntu.tar")
-	_, err = cmd.Output()
-	if err != nil {
-		log.Fatalf("Specstest root readonly test: export image error, %v", err)
-	}
-
-	cmd = exec.Command("/bin/sh", "-c", "mkdir -p ./../../source/rootfs_rootconfig")
-	_, err = cmd.Output()
-	if err != nil {
-		log.Fatalf("Specstest root readonly test: create rootfs dir error, %v", err)
-	}
-
-	cmd = exec.Command("/bin/sh", "-c", "tar -C ./../../source/rootfs_rootconfig -xf ubuntu.tar")
-	_, err = cmd.Output()
-	if err != nil {
-		log.Fatalf("Specstest root readonly test: create rootfs content error, %v", err)
-	}
+	fmt.Println("Host enviroment setting up for runc is already!")
 
 	var filePath string
 	filePath = "config.json"
@@ -89,6 +43,7 @@ func testRootReadonlyTrue() {
 
 	linuxspec.Spec.Root.Path = "./../../source/rootfs_rootconfig"
 	linuxspec.Spec.Root.Readonly = true
+	linuxspec.Spec.Process.Args[0] = "./root_readonly_true_guest"
 	err = configconvert.LinuxSpecToConfig(filePath, linuxspec)
 	//err = wirteConfig(filePath, linuxspec)
 	if err != nil {
@@ -96,12 +51,6 @@ func testRootReadonlyTrue() {
 	}
 	fmt.Println("Host enviroment for runc is already!")
 
-	/*
-		cmd = exec.Command("/bin/bash", "-c", "runc")
-		_, err = cmd.Output()
-		if err != nil {
-			log.Fatalf("Specstest root readonly test: start runc and test error, %v", err)
-		}*/
 }
 
 func main() {
