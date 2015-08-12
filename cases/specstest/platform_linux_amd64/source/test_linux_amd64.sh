e928a89506554914ec342f7e1ec2b6c6da1258bd
LinZhinan(Zen Lin) <linzhinan@huawei.com>
2015-08-12 23:46:54 +0800

    Correct platform_linux_amd64 testcase, adapt to framework.
    Signed-off-by: LinZhinan(Zen Lin) <linzhinan@huawei.com>

--- cases/specstest/platform_linux_amd64/source/test_linux_amd64.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd ./../../source/ ; runc
-if [ $? -eq 0 ]; then
-	echo "[YES]        linuxspec.Spec.Platform.OS = "linux"     linuxspec.Spec.Platform.Arch = "amd64"   passed"
-else 
-	echo "[NO]        linuxspec.Spec.Platform.OS = "linux"     linuxspec.Spec.Platform.Arch = "amd64"   failed"
-fi
-
-
