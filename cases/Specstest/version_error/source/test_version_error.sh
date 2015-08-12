4b9575892c958174e44b2a2a5bd98eb7c6c4d57f
LinZhinan(Zen Lin) <linzhinan@huawei.com>
2015-08-12 18:00:44 +0800

    Rename Benchmark to benchmark, Specstest to specstest.
    Signed-off-by: LinZhinan(Zen Lin) <linzhinan@huawei.com>

--- cases/Specstest/version_error/source/test_version_error.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd ./../../source ; runc
-if [ $? -eq 0 ]; then
-	echo "[NO]        Linuxspec.Spec.Version ==  errValue  failed"
-else 
-	echo "[Yes]        Linuxspec.Spec.Version == errValue   passed"
-fi
-
-
