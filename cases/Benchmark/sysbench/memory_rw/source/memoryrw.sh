81c58601e6bbd8c108f0faaae249e14a917131d9
LinZhinan(Zen Lin) <linzhinan@huawei.com>
2015-08-12 18:15:56 +0800

    Rename folder of Benchmark and Specstest.
    Signed-off-by: LinZhinan(Zen Lin) <linzhinan@huawei.com>

--- cases/Benchmark/sysbench/memory_rw/source/memoryrw.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-num_threads=$(cat /proc/cpuinfo | grep processor | wc -l)
-
-memory_test(){
-		sysbench --num-threads=${num_threads} --memory-block-size=1024 --memory-total-size=10G  --test=memory run
-		sysbench --num-threads=${num_threads} --memory-block-size=1024 --memory-total-size=100G  --test=memory run
-}
-
-memory_test
