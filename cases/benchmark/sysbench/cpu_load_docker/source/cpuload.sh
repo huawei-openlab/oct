a97c6522aeac40065c3f957bbb1ad3378915f203
wangkirin <wangqilin2@huawei.com>
2015-08-14 15:49:34 +0800

    divide sysbench/cpu_load into two folders

--- cases/benchmark/sysbench/cpu_load_docker/source/cpuload.sh
+++ cases/benchmark/sysbench/cpu_load_docker/source/cpuload.sh
@@ -3,7 +3,7 @@ num_threads=$(cat /proc/cpuinfo | grep processor | wc -l)
 
 
 cpu_test(){
-	for prime_num in 50000 500000  
+	for prime_num in 5000 50000  
 	do
 		sysbench --num-threads=1 --cpu-max-prime=${prime_num} --test=cpu run
 		sysbench --num-threads=${num_threads} --cpu-max-prime=${prime_num} --test=cpu run
