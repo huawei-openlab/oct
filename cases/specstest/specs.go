bdb1164f9720f439b39b2d15dae971ea7838a6ba
Wang Qilin <wangqilin2@huawei.com>
2015-08-27 10:00:28 +0800

    add new version linux resource test case

--- cases/specstest/specs.go
+++ cases/specstest/specs.go
@@ -18,11 +18,12 @@ import (
 	"io/ioutil"
 	"log"
 
-	"github.com/huawei-openlab/oct/cases/specstest/cases/specplatform"
-	"github.com/huawei-openlab/oct/cases/specstest/cases/specroot"
-	"github.com/huawei-openlab/oct/cases/specstest/cases/linuxnamespace"
-	"github.com/huawei-openlab/oct/cases/specstest/cases/specmount"
-	"github.com/huawei-openlab/oct/cases/specstest/cases/specversion"
+	// "github.com/huawei-openlab/oct/cases/specstest/cases/linuxnamespace"
+	"github.com/huawei-openlab/oct/cases/specstest/cases/linuxresources"
+	// "github.com/huawei-openlab/oct/cases/specstest/cases/specmount"
+	// "github.com/huawei-openlab/oct/cases/specstest/cases/specplatform"
+	// "github.com/huawei-openlab/oct/cases/specstest/cases/specroot"
+	// "github.com/huawei-openlab/oct/cases/specstest/cases/specversion"
 	"github.com/huawei-openlab/oct/cases/specstest/hostenv"
 )
 
@@ -32,44 +33,52 @@ func main() {
 	if err != nil {
 		log.Fatalf(" Pull image error, %v", err)
 	}
-	linuxnamespace.TestSuiteNP.Run()
-	result := linuxnamespace.TestSuiteNP.GetResult()
+	// linuxnamespace.TestSuiteNP.Run()
+	// result := linuxnamespace.TestSuiteNP.GetResult()
 
-	err = ioutil.WriteFile("namespace_out.json", []byte(result), 0777)
+	// err = ioutil.WriteFile("namespace_out.json", []byte(result), 0777)
+	// if err != nil {
+	// 	log.Fatalf("Write namespace out file error,%v\n", err)
+	// }
+
+	// // spec.version test
+	// specversion.TestSuiteVersion.Run()
+	// result = specversion.TestSuiteVersion.GetResult()
+
+	// err = ioutil.WriteFile("Version_out.json", []byte(result), 0777)
+	// if err != nil {
+	// 	log.Fatalf("Write version out file error,%v\n", err)
+	// }
+
+	// // spec.mount test
+	// specmount.TestSuiteMount.Run()
+	// result = specmount.TestSuiteMount.GetResult()
+	// err = ioutil.WriteFile("Mount_out.json", []byte(result), 0777)
+	// if err != nil {
+	// 	log.Fatalf("Write mount out file error,%v\n", err)
+	// }
+
+	// specroot.TestSuiteRoot.Run()
+	// result = specroot.TestSuiteRoot.GetResult()
+
+	// err = ioutil.WriteFile("Root_out.json", []byte(result), 0777)
+	// if err != nil {
+	// 	log.Fatalf("Write Root out file error,%v\n", err)
+	// }
+
+	// specplatform.TestSuitePlatform.Run()
+	// result = specplatform.TestSuitePlatform.GetResult()
+	// err = ioutil.WriteFile("Platform_out.json", []byte(result), 0777)
+	// if err != nil {
+	// 	log.Fatalf("Write Platform out file error,%v\n", err)
+	// }
+
+	//linux resources test
+	linuxresources.TestSuiteLinuxResources.Run()
+	result := linuxresources.TestSuiteLinuxResources.GetResult()
+	err = ioutil.WriteFile("LinuxResources_out.json", []byte(result), 0777)
 	if err != nil {
-		log.Fatalf("Write namespace out file error,%v\n", err)
-	}
-
-	// spec.version test
-	specversion.TestSuiteVersion.Run()
-	result = specversion.TestSuiteVersion.GetResult()
-
-	err = ioutil.WriteFile("Version_out.json", []byte(result), 0777)
-	if err != nil {
-		log.Fatalf("Write version out file error,%v\n", err)
-	}
-
-	// spec.mount test
-	specmount.TestSuiteMount.Run()
-	result = specmount.TestSuiteMount.GetResult()
-	err = ioutil.WriteFile("Mount_out.json", []byte(result), 0777)
-	if err != nil {
-		log.Fatalf("Write mount out file error,%v\n", err)
-	}
-
-	specroot.TestSuiteRoot.Run()
-	result = specroot.TestSuiteRoot.GetResult()
-
-	err = ioutil.WriteFile("Root_out.json", []byte(result), 0777)
-	if err != nil {
-		log.Fatalf("Write Root out file error,%v\n", err)
-	}
-
-	specplatform.TestSuitePlatform.Run()
-	result = specplatform.TestSuitePlatform.GetResult()
-	err = ioutil.WriteFile("Platform_out.json", []byte(result), 0777)
-	if err != nil {
-		log.Fatalf("Write Platform out file error,%v\n", err)
+		log.Fatalf("Write LinuxResources out file error,%v\n", err)
 	}
 
 }
