d6ccdbe08ae9fcedeb678cf14e49ad0233eecabc
LinZhinan(Zen Lin) <linzhinan@huawei.com>
2015-09-01 20:31:16 +0800

    Enable detech the runc, judge to download runc or not, and set runc env to $PATH.
    Signed-off-by: LinZhinan(Zen Lin) <linzhinan@huawei.com>

--- tools/specstest/hostenv/host_runc.go
+++ tools/specstest/hostenv/host_runc.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 func exists(name string) bool {
@@ -16,18 +17,37 @@ func exists(name string) bool {
 	return true
 }
 
-func upateRev(Rev string, repo string) (string, error) {
-	fmt.Println("Rev : %v", Rev)
-	goPath := os.Getenv("GOPATH")
-	path := goPath + "/src/github.com/opencontainers/" + repo
-	if !exists(path) {
-		log.Fatalf("In hostenv path: %v is not exist", path)
+//https://github.com/opencontainers/specs.git
+func cloneDeps(path string, repo string) error {
+	err := os.MkdirAll(path, os.ModePerm)
+	if err != nil {
+		log.Fatal("CloneDeps create path err = %v", err)
 	}
 
-	/*	err := os.Chdir(path)
+	cloneString := "https://github.com/opencontainers/" + repo + ".git"
+	cmd := exec.Command("git", "clone", cloneString)
+	cmd.Stderr = os.Stderr
+	//cmd.Stdout = os.Stdout
+	cmd.Stdin = os.Stdin
+	cmd.Dir = path
+	_, err = cmd.Output()
+	if err != nil {
+		log.Fatalf("CloneDeps git clone %v err", cloneString)
+	}
+	return nil
+}
+
+func upateRev(Rev string, repo string) (string, error) {
+	fmt.Printf("Rev : %v", Rev)
+	goPath := os.Getenv("GOPATH")
+	path := goPath + "/src/github.com/opencontainers/" + repo
+	pPath := goPath + "/src/github.com/opencontainers/"
+	if !exists(path) {
+		err := cloneDeps(pPath, repo)
 		if err != nil {
-			log.Fatalf("In Hostenv chdir to path : %v err", path)
-		}*/
+			return path, err
+		}
+	}
 
 	cmd := exec.Command("git", "checkout", Rev)
 	cmd.Stderr = os.Stderr
@@ -64,5 +84,18 @@ func UpateRuncRev(runcRev string) error {
 	if err != nil {
 		log.Fatalf("UpateRuncRev make install runc err : %v", err)
 	}
+	err = setRuncEnv()
 	return err
+}
+
+func setRuncEnv() error {
+	runcPath := "/usr/local/bin/"
+	result := os.Getenv("PATH")
+	if !strings.Contains(result, runcPath) {
+		newPath := runcPath + ":" + result
+		if err := os.Setenv("PATH", newPath); err != nil {
+			log.Fatalf("SetRuncEnv err %v", err)
+		}
+	}
+	return nil
 }
