d2b4a8b98b8d5f813155a83dd10969ad695c0156
wangkirin <wangqilin2@huawei.com>
2015-09-11 22:24:19 +0800

    divide clean runc into 2 parts
    Signed-off-by: wangkirin <wangqilin2@huawei.com>

--- tools/specsValidator/adaptor/runc_clean.go
+++ tools/specsValidator/adaptor/runc_clean.go
@@ -6,16 +6,26 @@ import (
 )
 
 func CleanRunc() {
+	KillRunc()
+	DeleteRun()
+}
+
+func KillRunc() {
 	cmd := exec.Command("/bin/bash", "-c", "kill -9 `ps -ef|grep runc|grep -v grep|awk '{print $2}'`")
 	_, err := cmd.Output()
 	if err != nil {
-		log.Fatalf("[clean runc] kill process error , %v", err)
+		log.Printf("[clean runc] kill process error , %v", err)
 	}
-	cmd = exec.Command("/bin/bash", "-c", "rm -r /run/oci/specsValidator")
-	_, err = cmd.Output()
-	if err != nil {
-		log.Fatalf("[clean runc] delete folder error , %v", err)
+}
+
+func DeleteRun() {
+	_, err := exec.Command("/bin/bash", "-c", "ls /run/oci/specsValidator").Output()
+	if err == nil {
+		cmd := exec.Command("/bin/bash", "-c", "rm -r /run/oci/specsValidator")
+		_, err = cmd.Output()
+		if err != nil {
+			log.Printf("[clean runc] delete folder error , %v", err)
+		}
 	}
-	log.Println("clean runc success")
 
 }
