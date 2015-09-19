89bd7dd107c139e7654779a6d4102e454c5f956c
LinZhinan(Zen Lin) <linzhinan@huawei.com>
2015-09-19 11:15:25 +0800

    Add creating func of minimunset of objs(linuxspec and linuxruntime), can be called in cases, to avoid repeat init of structs.
    Signed-off-by: LinZhinan(Zen Lin) <linzhinan@huawei.com>

---
+++ tools/specsValidator/utils/specsinit/minimumlinuxspecinit_v0.1.1.go
@@ -0,0 +1,44 @@
+// +build v0.1.1
+
+package specsinit
+
+import (
+	"github.com/opencontainers/specs"
+	"runtime"
+)
+
+func SetLinuxspecMinimum() specs.LinuxSpec {
+	var linuxSpec specs.LinuxSpec = specs.LinuxSpec{
+		Spec: specs.Spec{
+			Version: "0.1.0",
+			Platform: specs.Platform{
+				OS:   runtime.GOOS,
+				Arch: runtime.GOARCH,
+			},
+			Root: specs.Root{
+				Path:     "rootfs",
+				Readonly: true,
+			},
+			Process: specs.Process{
+				Terminal: false,
+				User: specs.User{
+					UID:            0,
+					GID:            0,
+					AdditionalGids: nil,
+				},
+				Args: []string{""},
+				Env:  []string{""},
+				Cwd:  "",
+			},
+			Hostname: "zenlinHost",
+			Mounts: []specs.MountPoint{
+				{
+					Name: "proc",
+					Path: "/proc",
+				},
+			},
+		},
+	}
+
+	return linuxSpec
+}
