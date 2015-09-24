8aaf8052ed0af39bdcd70afcf4149a8c426e0486
linzhinan <linzhinan@huawei.com>
2015-09-24 16:10:18 +0800

    Update startRunc to drop err output from runc in controller.
    Signed-off-by: linzhinan <linzhinan@huawei.com>

--- tools/specsValidator/adaptor/runc_start_v0.1.1.go
+++ tools/specsValidator/adaptor/runc_start_v0.1.1.go
@@ -3,11 +3,12 @@
 package adaptor
 
 import (
-	"os"
+	"io/ioutil"
+	"log"
 	"os/exec"
 )
 
-func StartRunc(configFile string, runtimeFile string) (string, error) {
+/*func StartRunc(configFile string, runtimeFile string) (string, error) {
 	var cmd *exec.Cmd
 	if configFile == "" && runtimeFile == "" {
 		cmd = exec.Command("runc", "start")
@@ -27,4 +28,43 @@ func StartRunc(configFile string, runtimeFile string) (string, error) {
 	cmd.Dir = "./"
 	outPut, err := cmd.Output()
 	return string(outPut), err
+}*/
+
+func StartRunc(configFile string, runtimeFile string) (string, error) {
+	var cmd *exec.Cmd
+	if configFile == "" && runtimeFile == "" {
+		cmd = exec.Command("runc", "start")
+	} else {
+		if configFile == "" {
+			cmd = exec.Command("runc", "start", "-r", runtimeFile)
+		} else if runtimeFile == "" {
+			cmd = exec.Command("runc", "start", "-c", configFile)
+		} else {
+			cmd = exec.Command("runc", "start", "-c", configFile, "-r", runtimeFile)
+		}
+	}
+
+	// cmd.stdin = os.Stdin
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		log.Fatal("stderr err %v", err)
+	}
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatalf("stdout err %v", err)
+	}
+
+	var retStr string
+	err = cmd.Start()
+	if err != nil {
+		retb, _ := ioutil.ReadAll(stderr)
+		retStr = string(retb)
+		// stdoutReader.ReadRune()
+	} else {
+		retb, _ := ioutil.ReadAll(stdout)
+		retStr = string(retb)
+	}
+
+	return retStr, err
 }
